7d982f3238d2340f627f20d6cadf4e68176fc2e3
Chris Austin <caustin.adwan@gmail.com>
2017-12-05 15:02:06 -0800

    Bug fix: Set 'react' and 'material-ui' as 'peerDependencies' (#9)
    This commit fixes an issue where we were bundling up 'react' and
    'material-ui'. This was bad as since we expect our
    component/module/package to be consumed we can be certain that the
    consumers already have the 'react' and 'material-ui' modules as
    dependencies. As a result duplicate modules were loaded which caused
    problems such as MUI components/elements to lose their styling in SSR
    use cases.
    
    We fix the issue by setting the 'react' and 'material-ui' modules as
    'peerDependencies' to our package. We do add them as 'devDependencies'
    so that work can be done on our component directly in this repo/package
    folder. To ensure that we don't bundle 'react' and 'material-ui' when we
    install them as 'devDependencies' we use 'webpack-node-externals' to
    cause webpack to exclude everything in 'node_modules' from being bundled
    and then whitelisting our few actual 'dependencies'.
    
    This commit fixes bug: #7
    	modified:   package.json
    	modified:   test/test.jsx.snap
    	modified:   webpack.config.js
    	modified:   yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: path.join(__dirname, './src/index.js'),
@@ -9,6 +10,15 @@ module.exports = {
     // Definition
     libraryTarget: 'umd',
   },
+  // 'externals' provides us a way of excluding dependencies from the output of the bundle. In this
+  // case we use 'webpack-node-externals' to build/exclude all dependencies as found in
+  // 'node_modules' and then we whitelist our few 'dependencies' found in 'package.json'. This way
+  // we don't bundle up 'material-ui' and 'react' causing module duplication (which causes things
+  // to break) while still allowing our component to be consumed off the shelf by bundling our few
+  // 'dependencies'.
+  externals: [nodeExternals({
+    whitelist: ['autosuggest-highlight', 'prop-types', 'react-autosuggest'],
+  })],
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: [/node_modules/] },
