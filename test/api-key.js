3a243527d50cb7763402a567e28b3b22ace1e627
Chris Austin <caustin.adwan@gmail.com>
2018-03-21 19:04:36 +0000

    Upgrade: Rev 'downshift' from 1.22.5 to 1.31.2
    This commit revs the 'downshift' package version form 1.22.5 to 1.31.2.
    After reading the release docs it didn't appear that there was any
    breaking changes. Tests passed and functionality appears okay.
    	modified:   test/api-key.js
    	modified:   test/test.jsx.snap
    	modified:   yarn.lock

--- test/api-key.js
+++ test/api-key.js
@@ -1,2 +1,4 @@
 // Set your Google API key so that it can be used for the tests
-export default process.env.GOOGLE_API_KEY || 'Replace with your Google API key'
+export default process.env.GOOGLE_API_TEST_KEY ||
+  process.env.GOOGLE_API_KEY ||
+  'Replace with your Google API key'
